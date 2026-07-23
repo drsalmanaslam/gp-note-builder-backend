50011a0864a24df1de4ffb507de84ed22ec516f4
drsalmanaslam <drsalmanaslam123@gmail.com>
2026-07-23 20:35:31 +0100

    Disable stripe routes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.util import get_remote_address
 from slowapi.errors import RateLimitExceeded
-from app.routers import stripe_routes
+# from app.routers import stripe_routes
 from app.routers import categories
 from app.routers import notes
 from fastapi.middleware.cors import CORSMiddleware
@@ -68,7 +68,7 @@ app.include_router(auth.router)
 app.include_router(templates.router)
 app.include_router(notes.router)
 app.include_router(categories.router)
-app.include_router(stripe_routes.router)
+# app.include_router(stripe_routes.router)
 
 @app.get("/")
 def read_root():
